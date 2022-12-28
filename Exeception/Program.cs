4acb18ae9b29babced6911ef99ecebecc7809151
907460 <907460@LPTEMP907460>
2022-12-28 10:33:57 +0300

    added or not

---
+++ Exeception/Program.cs
@@ -0,0 +1,37 @@
+﻿using System.IO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Exeception
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            try
+            {
+                StreamReader streamReader = new StreamReader(@"C:\Test\Try.txt"); //file to read  
+                Console.WriteLine(streamReader.ReadToEnd()); //read all Char in file
+                streamReader.Close(); //close reading
+                Console.ReadLine();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);// To show the exception message
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine(ex.StackTrace);//provide a call stack to the line number where exception occured.
+                Console.ReadLine();
+
+            }
+        }
+    }
+}               
+
+        
+
